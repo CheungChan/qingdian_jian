c190fde3ccfd10dbd945f4b0e2361c8a217fc713
陈章 <1377699408@qq.com>
2017-12-05 18:15:01 +0800

    bug fix

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@
 # 杀
 pgrep gunicorn|xargs kill
 # 起
-/root/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian:wsgi
+/root/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian.wsgi
