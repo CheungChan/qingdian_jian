55e93ebc08a45f631bb0617e0c46a7ce2d43e4e6
陈章 <1377699408@qq.com>
2017-12-06 10:26:59 +0800

    重构

--- restart.sh
+++ restart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # 杀
-pgrep gunicorn|xargs kill
+APP_NAME='qingdian_jian'
+ps aux|grep ${APP_NAME}|grep -v 'grep'|awk '{print $2}'|xargs kill
 # 起
 /root/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian.wsgi
