ddc9403d4f8307635563d4dc9aa6b1a7e7f883bd
陈章 <1377699408@qq.com>
2018-01-01 20:58:04 +0800

    加入访问日志

--- jian/views/tuijian.py
+++ jian/views/tuijian.py
@@ -15,6 +15,7 @@ def cids_by_uid(request):
     uid = request.GET.get('uid')
     n = request.GET.get('n', 20)
     uid, n = trans_int(uid, n)
+    logger.info(f'访问cids_by_uid?uid={uid}&n={n}')
     if uid is None or n is None:
         j = {'status': -1, 'data': []}
         return JsonResponse(j, safe=False)
@@ -30,6 +31,7 @@ def jian_history(request):
     page_size = request.GET.get('page_size', 10)
     page_no = request.GET.get('page_no', 1)
     uid, page_size, page_no = trans_int(uid, page_size, page_no)
+    logger.info(f'访问jian_history?uid={uid}&page_size={page_size}&page_no={page_no}')
     if page_size is None or page_no is None:
         j = {'status': -1, 'data': []}
         return JsonResponse(j, safe=False)
