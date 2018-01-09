9a057c10c439555c56390864ca09a555222c23b0
陈章 <1377699408@qq.com>
2018-01-09 19:53:57 +0800

    shell

--- restart.sh
+++ restart.sh
@@ -2,6 +2,10 @@
 
 # 杀
 APP_NAME='qingdian_jian'
+echo "stopping$2"
 ps aux|grep ${APP_NAME}|grep -v 'grep'|awk '{print $2}'|xargs kill
+echo "stopped"
+echo "starting"
 # 起
 /root/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian.wsgi
+echo "started"
