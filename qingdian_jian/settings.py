779a69562f4dd0097cf0f33b09679000af5e7bd6
陈章 <1377699408@qq.com>
2018-01-09 20:04:19 +0800

    调整引擎比例

--- qingdian_jian/settings.py
+++ qingdian_jian/settings.py
@@ -164,7 +164,7 @@ LOGGING = {
     },
     'formatters': {
         'standard': {
-            'format': '%(asctime)s [%(name)s.%(funcName)s:%(lineno)d] [%(levelname)s]: %(message)s'},
+            'format': '(%(asctime)s) [%(name)s.%(funcName)s:%(lineno)d] [%(levelname)s]: %(message)s'},
     },
     'loggers': {
         'jian': {
