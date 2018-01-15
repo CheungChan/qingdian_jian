9a20d01734abcee160a0f5e1bc6a6bec7732a1ca
陈章 <1377699408@qq.com>
2018-01-15 17:44:54 +0800

    django增加静态文件支持

--- qingdian_jian/urls.py
+++ qingdian_jian/urls.py
@@ -5,9 +5,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jian/', include('jian.urls')),
     path('jian/kan/', include('kan.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
