2b510805062425198bd5dc59513bd798920a0054
陈章 <1377699408@qq.com>
2018-01-24 19:54:25 +0800

    restart.sh

--- restart.sh
+++ restart.sh
@@ -2,10 +2,17 @@
 
 # 杀
 APP_NAME='qingdian_jian'
-echo "stopping $APP_NAME"
-ps aux|grep ${APP_NAME}|grep -v 'grep'|awk '{print $2}'|xargs kill
-echo "stopped"
+echo "尝试停止 $APP_NAME"
+echo "运行中的任务:"
+ps aux|grep ${APP_NAME}|grep -v 'grep'
+if [ $? -ne 0 ]
+then
+    echo "**** $APP_NAME 未运行"
+else
+    ps aux|grep ${APP_NAME}|grep -v 'grep'|awk '{print $2}'|xargs kill
+    echo "**** $APP_NAME 终止成功"
+fi
 # 起
-echo "starting $APP_NAME"
-/root/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian.wsgi
-echo "started"
+echo "尝试开启 $APP_NAME"
+~/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian.wsgi
+echo "**** $APP_NAME 启动成功"
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-APP_NAME='qingdian_jian'
-
-# 起
-echo "starting $APP_NAME"
-/root/envs/qingdian_jian/bin/gunicorn -D -w 4 -b :9000 qingdian_jian.wsgi
-echo "started"
