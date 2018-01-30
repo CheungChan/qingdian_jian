721befee894b638960868c68c5ccd08ebac8cb12
陈章 <1377699408@qq.com>
2018-01-30 17:08:12 +0800

    restart.sh

--- restart.sh
+++ restart.sh
@@ -10,10 +10,16 @@ then
     echo '$WORKON_HOME 环境变量未设置,请安装virtualenv virtualenvwrapper 并设置环境变量$WORKON_HOME'
     exit -1
 fi
+VIRTUAL_ENV_PYTHON_HOME="$WORKON_HOME"/"$APP_NAME"
+if [ !-d "$VIRTUAL_ENV_PYTHON_HOME" ]
+then
+    echo "$VIRTUAL_ENV_PYTHON_HOME 虚拟环境未创建,现在开始创建虚拟环境"
+    mkvirtualenv "$APP_NAME"
+fi
 
 # 装依赖
 echo '安装依赖'
-"$WORKON_HOME"/"$APP_NAME"/bin/pip install -r requirements.txt
+"$VIRTUAL_ENV_PYTHON_HOME"/bin/pip install -r requirements.txt
 echo "依赖安装完成"
 
 # 杀
@@ -30,5 +36,5 @@ fi
 
 # 起
 echo "尝试开启 $APP_NAME"
-"$WORKON_HOME"/"$APP_NAME"/bin/gunicorn -D -w 4 -b :"$PORT" "$APP_NAME".wsgi
+"$VIRTUAL_ENV_PYTHON_HOME"/bin/gunicorn -D -w 4 -b :"$PORT" "$APP_NAME".wsgi
 echo "**** $APP_NAME 启动成功"
