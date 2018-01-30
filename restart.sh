b90ab5997837cc728a00bc3b586cdc7decf7da0d
陈章 <1377699408@qq.com>
2018-01-30 17:00:27 +0800

    restart.sh

--- restart.sh
+++ restart.sh
@@ -4,11 +4,17 @@
 APP_NAME='qingdian_jian'
 PORT=9000
 
+# 检测
+if [ -z "$WORKON_HOME" ]
+then
+    echo '$WORKON_HOME 环境变量未设置,请安装virtualenv virtualenvwrapper 并设置环境变量$WORKON_HOME'
+    exit -1
+fi
 
 # 装依赖
 echo '安装依赖'
-~/envs/"$APP_NAME"/bin/pip install -r requirements.txt
-
+"$WORKON_HOME"/"$APP_NAME"/bin/pip install -r requirements.txt
+echo "依赖安装完成"
 
 # 杀
 echo "尝试停止 $APP_NAME"
@@ -22,8 +28,7 @@ else
     echo "**** $APP_NAME 终止成功"
 fi
 
-
 # 起
 echo "尝试开启 $APP_NAME"
-~/envs/"$APP_NAME"/bin/gunicorn -D -w 4 -b :"$PORT" "$APP_NAME".wsgi
+"$WORKON_HOME"/"$APP_NAME"/bin/gunicorn -D -w 4 -b :"$PORT" "$APP_NAME".wsgi
 echo "**** $APP_NAME 启动成功"
