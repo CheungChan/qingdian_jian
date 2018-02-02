43b3095fa197d46978ca8eb5a81e1cc48959e851
陈章 <1377699408@qq.com>
2018-02-02 15:29:07 +0800

    脚本打印日志

--- jian/engines/content_based_engine.py
+++ jian/engines/content_based_engine.py
@@ -13,7 +13,8 @@ from datetime import datetime
 import jieba
 
 from jian.engines.base_engine import BaseEngine
-from qingdian_jian.utils import override, get_mongo_collection
+from qingdian_jian.utils import override
+from jian import mongo_models
 
 logger = logging.getLogger(__name__)
 pwd = os.path.dirname(os.path.abspath(__name__))
@@ -35,24 +36,29 @@ class ContentBasedEngine(BaseEngine):
         """
         if self.process.len_tracked == 0:
             return []
-        result: List[List[int, float, str]] = []
+        d = {}
         for cid in self.process.tracked_cids:
             # 内容最相似的已经离线计算好,存到了redis里面,直接取出来.
-            cid_simi_list = self.get_cached_similarity(cid)
+            cid_simi_list = mongo_models.ContentSimilarityOffline.get_cached_similarity_by_cid(cid)
             if len(cid_simi_list) == 0:
                 continue
-            result.extend(cid_simi_list)
+            for cid, simi in cid_simi_list:
+                d.setdefault(cid, [0, 0.0])  # [sim的累加,次数]
+                d[cid][0] += simi
+                d[cid][1] += 1
         logger.info('相似度计算后')
         # 过滤
-        d = {}
-        logger.info(f'len_result={len(result)}')
-        for cid, simi in result:
-            d.setdefault(cid, [0, 0.0])  # [sim的累加,次数]
-            d[cid][0] += simi
-            d[cid][1] += 1
-        logger.info('过滤后')
+
+        # 经过日志发现,摊平特别浪费时间,随着用户喜欢的增加,要摊平的result长度飙升.比如我的result长度现在是2千万多.所以考虑将计算过的d
+        # 缓存起来.
+        # 具体方案: 将key为uid,值为len(tracked_ids),len(simi_per_cid),d缓存起来.当len(tracked_ids),len(simi_per_cid)变大时,
+        # 将d取出,在d基础上做更新,更新的内容是作为条件计算.这样只需要计算没算过的那部分result就可以了.
+        # d = cached_d + (len_simi_now - len_simi_cache) * len_tracked_cached +
+        # (len_tracked_now - len_tracked_cached) * len_simi_now
+        logger.info('摊平后')
         for f_id in self.process.fitering_cids:
             d.pop(f_id, None)
+        logger.info('过滤后')
         result = []
         for cid, sumsim_count_list in d.items():
             result.append([cid, sumsim_count_list[0] / sumsim_count_list[1]])
@@ -65,10 +71,3 @@ class ContentBasedEngine(BaseEngine):
         for r in result:
             r.append(self.__class__.__name__)
         return result
-
-    @classmethod
-    @lru_cache(None)
-    def get_cached_similarity(cls, cid):
-        db = get_mongo_collection("content_similarity_offline")
-        cached_value = db.find_one({'cid': cid})
-        return cached_value['cid2_sim'] if cached_value else []
