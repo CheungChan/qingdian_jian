c552e0aa19b7a70c74f55c6bc35270f9b588d91f
香饽饽 <1377699408@qq.com>
2018-03-19 12:20:51 +0800

    sql用records

--- sql_script_template.py
+++ sql_script_template.py
@@ -1,51 +1,22 @@
 import os
 
 import logzero
-import pymysql
+import records
 
 from qingdian_jian.settings import DEBUG
 
 logfile = f"/tmp/{os.path.basename(__file__)}.log"
 logzero.logfile(logfile, encoding='utf-8', maxBytes=500_0000, backupCount=3)
 print(f'脚本DEBUG={DEBUG}')
-test_db = {
-    'db': 'qdbuluo',
-    'host': '10.10.6.2',
-    'user': 'develop',
-    'password': '123-qwe',
-    'charset': 'utf8mb4',
-}
-prod_db = {
-    'db': 'qdbuluo',
-    'host': '10.10.6.6',
-    'port': 3306,
-    'user': 'develop',
-    'password': '123^%$-qwe',
-    'charset': 'utf8mb4',
-}
-BL_MYSQL_CONF = test_db if DEBUG else prod_db
-print(BL_MYSQL_CONF)
-connection = None
-
-
-def get_connection():
-    global connection
-    if not connection:
-        connection = pymysql.connect(**BL_MYSQL_CONF)
-    return connection
-
-
-def executesql_and_fetchall(sql: str, *args):
-    conn = get_connection()
-    with conn.cursor() as cursor:
-        cursor.execute(sql, args)
-        return cursor.fetchall()
+TEST_MYSQL_URL = 'mysql://10.10.6.2/qdbuluo?user=develop&password=123-qwe&charset=utf8mb4'
+PROD_MYSQL_URL = 'mysql://10.10.6.6/qdbuluo?user=develop&password=123^%$-qwe&charset=utf8mb4'
+MYSQL_URL = TEST_MYSQL_URL if DEBUG else PROD_MYSQL_URL
+db = records.Database(MYSQL_URL)
 
 
 def main():
-    sql1 = 'SELECT id FROM contents WHERE contents_refter_type = %s'
-    cids = executesql_and_fetchall(sql1, (14,))
-    print(cids)
+    tags = db.query('select * from tag limit 10').as_dict()
+    print(tags)
 
 
 if __name__ == '__main__':
